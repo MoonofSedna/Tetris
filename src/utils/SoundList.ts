6d0e4a42605db774528d8206e5c4d7f8eea6b30f
Bethania Troconis <60520922+MoonofSedna@users.noreply.github.com>
2023-04-27 16:04:41 -0500

    sound list updated

--- src/utils/SoundList.ts
+++ src/utils/SoundList.ts
@@ -1,13 +1,15 @@
+import Success from "../assets/sounds/success.mp3";
+import Rotate from "../assets/sounds/rotate.mp3";
+import LevelUp from "../assets/sounds/level-up.mp3";
+import FastDrop from "../assets/sounds/fast-drop.mp3";
+import GameSound from "../assets/sounds/game-sound.mp3";
+import Pause from "../assets/sounds/pause.mp3";
+
 export const sounds = {
-  SUCCESS:
-    "src/assets/sounds/success.mp3",
-  ROTATE:
-    "src/assets/sounds/rotate.mp3",
-  LEVEL_UP:
-    "src/assets/sounds/level-up.mp3",
-  FAST_DROP:
-    "src/assets/sounds/fast-drop.mp3",
-  GAME_SOUND:
-    "src/assets/sounds/game-sound.mp3",
-  PAUSE: "src/assets/sounds/pause.mp3",
+  SUCCESS: Success,
+  ROTATE: Rotate,
+  LEVEL_UP: LevelUp,
+  FAST_DROP: FastDrop,
+  GAME_SOUND: GameSound,
+  PAUSE: Pause,
 };
